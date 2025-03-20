d36fbfdd4851274482f215628d0ad68674b647e7
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2025-03-20 12:36:48 +0000

    get_phone function was reimplemented

--- get_phone.py
+++ get_phone.py
@@ -1,16 +1,20 @@
+from classes import AddressBook
+
 def input_error(func):
     def inner(*args, **kwargs):
         try:
             if len(args[0]) != 1:
                 raise ValueError
             return func(*args, **kwargs)
-        except KeyError:
-            return "Such a contact doesn't exist."
         except ValueError:
             return "Give me a name, please."
 
     return inner
 
 @input_error
-def get_phone(args, contacts):
-    return contacts[args[0]]
+def get_phone(args, book: AddressBook):
+    name, *_ = args
+    record = book.find(name)
+    if not record:
+        return "Such a contact doesn't exist."
+    return book.find(name).phones
